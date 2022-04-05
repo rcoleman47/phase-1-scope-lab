d9e49d8f15a2464937a56bcd8f016837afd96922
ryanadams <rcoleman.adams@gmail.com>
2022-04-05 11:37:18 -0500

    commit tree a5127685b0424fce525740990788b9b7b9197d5f

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(name){
+    customerName = "BOB";
+    return customerName;
+};
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+};
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+};
+
+const leastFavoriteCustomer = 'charles';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'carl';
+    return leastFavoriteCustomer;
+};
